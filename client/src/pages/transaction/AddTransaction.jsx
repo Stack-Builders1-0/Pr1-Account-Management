243198c3c6a7e3e85e58b2875c0435af2f7bd907
THANU16 <93480395+THANU16@users.noreply.github.com>
2023-07-18 10:09:39 +0530

    Update AddTransaction.jsx

--- client/src/pages/transaction/AddTransaction.jsx
+++ client/src/pages/transaction/AddTransaction.jsx
@@ -7,7 +7,7 @@ import CashSalesTable from "./CashSales";
 import CreditSalesTable from "./CreditSales";
 import AdvancedBPSalesTable from "./AdvancedBP";
 import AdvancedAPSalesTable from "./AdvancedAP";
-
+// change 
 function AddTransaction() {
   const [selectedTab, setSelectedTab] = useState("");
   const [showTransaction, setShowTransaction] = useState(false);
