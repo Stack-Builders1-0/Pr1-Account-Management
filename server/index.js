4023b2e3d6e7f7fdc7991828898cce87611ea913
THANU16 <93480395+THANU16@users.noreply.github.com>
2023-07-20 03:43:27 +0530

    Revert "create expenses page"

--- server/index.js
+++ server/index.js
@@ -1,35 +1,38 @@
-var http = require("http");
-const express = require("express");
+var http = require('http');
+const express = require('express');
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 
-const employee = require("./routerComponent/pages/employee");
-const customer = require("./routerComponent/pages/customer");
-const cashSale = require("./routerComponent/pages/cashSale");
-const creditSale = require("./routerComponent/pages/creditSale");
-const advanceSaleAfterProduct = require("./routerComponent/pages/advanceSaleAfterProduct");
-const advanceSaleBeforProduct = require("./routerComponent/pages/advanceSaleBeforeProduct");
-const dashboard = require("./routerComponent//pages/dashboard");
-const login = require("./routerComponent/Authentication/login");
-const expenses = require("./routerComponent/pages/expenses");
-server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/plain");
-});
+const employee = require('./routerComponent/pages/employee');
+const customer = require('./routerComponent/pages/customer');
+const cashSale = require('./routerComponent/pages/cashSale');
+const creditSale = require('./routerComponent/pages/creditSale');
+const advanceSaleAfterProduct = require('./routerComponent/pages/advanceSaleAfterProduct');
+const advanceSaleBeforProduct = require('./routerComponent/pages/advanceSaleBeforeProduct');
+const dashboard = require('./routerComponent//pages/dashboard');
+const login = require('./routerComponent/Authentication/login');
+
+
+
+server = http.createServer ((req, res)=> {
+  res.setHeader('Content-Type', 'text/plain');
+})
 
 // Set up body parsing middleware
 app.use(express.json());
 
-app.use("/employee", employee);
-app.use("/customer", customer);
-app.use("/cashSale", cashSale);
-app.use("/creditSale", creditSale);
-app.use("/advanceSaleAP", advanceSaleAfterProduct);
-app.use("/advanceSaleBP", advanceSaleBeforProduct);
-app.use("/dashboard", dashboard);
-app.use("/login", login);
-app.use("/expenses", expenses);
+app.use('/employee', employee);
+app.use('/customer', customer);
+app.use('/cashSale', cashSale);
+app.use('/creditSale', creditSale);
+app.use('/advanceSaleAP', advanceSaleAfterProduct);
+app.use('/advanceSaleBP', advanceSaleBeforProduct);
+app.use('/dashboard', dashboard);
+app.use('/login',login);
 
 app.listen(5000, () => {
-  console.log("Listen port 5000");
-});
+    console.log("Listen port 5000");
+  });
+
+  
