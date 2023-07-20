0f9a09ae148da798947a16766b1082ab705a673f
Pairavi <pairavi.20@cse.mrt.ac.lk>
2023-07-20 16:24:11 +0530

    advancebp

--- client/src/pages/transaction/AdvanceBPTransaction.jsx
+++ client/src/pages/transaction/AdvanceBPTransaction.jsx
@@ -1,44 +1,102 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+import { Button, Form, Navbar, Container, Nav } from "react-bootstrap";
+import SettleAdvancedBP from "./SettleAdvancedBP";
+import Alert from "react-bootstrap/Alert";
 
 function AdvanceBPTransaction() {
   const [data, setData] = useState({
     customer_id: "",
     manual_invoice_id: "",
-    return_payment: "",
     description: "",
     billAmount: "",
     advanceAmount: "",
     discount: "",
     date: "",
-    employeeId: "",
   });
+
+  const [customerInfo, setCustomerInfo] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
+  const [searchedNic, setSearchedNic] = useState(false);
+  const [showAddForm, setShowAddForm] = useState(true);
+
+  const handleShowAddForm = () => {
+    setShowAddForm(true);
+  };
+
+  const handleShowSettleForm = () => {
+    setShowAddForm(false);
+  };
 
   const navigate = useNavigate();
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const formdata = new FormData();
-    formdata.append("customer_id", data.customer_id);
-    formdata.append("manual_invoice_id", data.manual_invoice_id);
-    formdata.append("return_payment", data.return_payment);
-    formdata.append("description", data.description);
-    formdata.append("billAmount", data.billAmount);
-    formdata.append("advanceAmount", data.advanceAmount);
-    formdata.append("discount", data.discount);
-    formdata.append("date", data.date);
-    formdata.append("employeeId", data.employeeId);
+  const handleSearch = () => {
+    const apiUrl = "http://localhost:5000/customer/filterCustomerNIC";
 
     axios
-      .post("http://localhost:5000/advancebppayment/add", formdata)
+      .post(apiUrl, { nic: data.nic_no })
       .then((res) => {
-        console.log(res);
-        navigate("/transaction");
+        const responseData = res.data;
+        console.log(responseData);
+        if (responseData.sucess && responseData.result.length > 0) {
+          // NIC number is valid and customer information is found
+          const customerData = responseData.result[0];
+          setCustomerInfo({
+            customerName: customerData.customer_name,
+            businessName: customerData.business_name,
+          });
+          setShowAlert(false);
+          setData({ ...data, customer_name: customerData.customer_name });
+          setData({ ...data, customer_id: customerData.customer_id });
+          setSearchedNic(true);
+        } else {
+          // NIC number is invalid or no customer found
+          setCustomerInfo(null);
+          setShowAlert(true);
+          setSearchedNic(false);
+        }
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const sessionToken = localStorage.getItem("sessionToken");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (searchedNic) {
+      const formdata = {
+        type_id: "as", // we manually set the type id of tha advancebp sale
+        manual_invoice_id: data.manual_invoice_id,
+        date: data.date,
+        customer_id: data.customer_id,
+        description: data.description,
+        bill_amount: data.billAmount,
+        discount: data.discount,
+        advanceAmount: data.advanceAmount,
+      };
+
+      axios
+        .post("http://localhost:5000/advancesalebp/add", formdata, {
+          headers: { Authorization: "key " + sessionToken },
+        })
+        .then((res) => {
+          navigate("/transaction");
+        })
+        .catch((err) => console.log(err));
+    } else {
+      alert(
+        "Please search for a valid NIC first before submitting the form or you have to register first"
+      );
+    }
+  };
+
+  const handleCancel = () => {
+    navigate("/transaction");
   };
 
   return (
@@ -47,111 +105,157 @@ function AdvanceBPTransaction() {
         <div className="d-flex flex-column align-items-center">
           <h2>AdvanceBP Payment Only</h2>
         </div>
-        <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicCustomerId">
-            <Form.Label>Customer ID</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter customer ID"
-              value={data.customer_id}
-              onChange={(e) =>
-                setData({ ...data, customer_id: e.target.value })
-              }
-            />
-          </Form.Group>
+        <Navbar sticky="top" bg="light" expand="md">
+          <Container>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link
+                  as={Link}
+                  to={"/transaction/advancebptransaction"}
+                  onClick={handleShowAddForm}
+                >
+                  Add
+                </Nav.Link>
 
-          <Form.Group className="mb-3" controlId="formBasicManualInvoiceId">
-            <Form.Label>Bill number</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter bill number"
-              value={data.manual_invoice_id}
-              onChange={(e) =>
-                setData({ ...data, manual_invoice_id: e.target.value })
-              }
-            />
-          </Form.Group>
+                <Nav.Link
+                  as={Link}
+                  to={"/transaction/advancebptransaction"}
+                  onClick={handleShowSettleForm}
+                >
+                  Settle
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
 
-          <Form.Group className="mb-3" controlId="formBasicReturnPayment">
-            <Form.Label>Return Payment</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter return payment"
-              value={data.return_payment}
-              onChange={(e) =>
-                setData({ ...data, return_payment: e.target.value })
-              }
-            />
-          </Form.Group>
+        {showAddForm && (
+          <>
+            <h4 className="text-center">Add Payment</h4>
+            <Form.Group className="mb-3" controlId="formBasicNicNo">
+              <Form.Label>Search Customer by NIC</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter NIC"
+                value={data.nic_no}
+                onChange={(e) => setData({ ...data, nic_no: e.target.value })}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter") {
+                    e.preventDefault(); // Prevent form submission
+                    handleSearch(); // Perform the search operation
+                  }
+                }}
+              />
+              <Button variant="primary" onClick={handleSearch}>
+                Search
+              </Button>
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicDescription">
-            <Form.Label>Description</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter description"
-              value={data.description}
-              onChange={(e) =>
-                setData({ ...data, description: e.target.value })
-              }
-            />
-          </Form.Group>
+            {showAlert && (
+              <Alert variant="danger">
+                No customer found with the provided NIC. Do you want to
+                register?{" "}
+                <Link to="/addcustomer" className="btn btn-primary">
+                  Add Customer
+                </Link>
+              </Alert>
+            )}
 
-          <Form.Group className="mb-3" controlId="formBasicBillAmount">
-            <Form.Label>Bill Amount</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter bill amount"
-              value={data.billAmount}
-              onChange={(e) => setData({ ...data, billAmount: e.target.value })}
-            />
-          </Form.Group>
+            {customerInfo && (
+              <div className="customer-info-box">
+                <h3>Customer Information</h3>
+                <p>
+                  <strong>Customer Name:</strong> {customerInfo.customerName}
+                </p>
+                <p>
+                  <strong>Business Name:</strong> {customerInfo.businessName}
+                </p>
+              </div>
+            )}
 
-          <Form.Group className="mb-3" controlId="formBasicAdvanceAmount">
-            <Form.Label>Advance Amount</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter advance amount"
-              value={data.advanceAmount}
-              onChange={(e) =>
-                setData({ ...data, advanceAmount: e.target.value })
-              }
-            />
-          </Form.Group>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group className="mb-3" controlId="formBasicManualInvoiceId">
+                <Form.Label>Bill number</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter bill number"
+                  value={data.manual_invoice_id}
+                  onChange={(e) =>
+                    setData({ ...data, manual_invoice_id: e.target.value })
+                  }
+                />
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicDiscount">
-            <Form.Label>Discount</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Enter discount"
-              value={data.discount}
-              onChange={(e) => setData({ ...data, discount: e.target.value })}
-            />
-          </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter description"
+                  value={data.description}
+                  onChange={(e) =>
+                    setData({ ...data, description: e.target.value })
+                  }
+                />
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicDate">
-            <Form.Label>Date</Form.Label>
-            <Form.Control
-              type="date"
-              placeholder="Enter date"
-              value={data.date}
-              onChange={(e) => setData({ ...data, date: e.target.value })}
-            />
-          </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicBillAmount">
+                <Form.Label>Bill Amount</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Enter bill amount"
+                  value={data.billAmount}
+                  onChange={(e) =>
+                    setData({ ...data, billAmount: e.target.value })
+                  }
+                />
+              </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicEmployeeId">
-            <Form.Label>Employee ID</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter employee ID"
-              value={data.employeeId}
-              onChange={(e) => setData({ ...data, employeeId: e.target.value })}
-            />
-          </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicAdvanceAmount">
+                <Form.Label>Advance Amount</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Enter advance amount"
+                  value={data.advanceAmount}
+                  onChange={(e) =>
+                    setData({ ...data, advanceAmount: e.target.value })
+                  }
+                />
+              </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
+              <Form.Group className="mb-3" controlId="formBasicDiscount">
+                <Form.Label>Discount</Form.Label>
+                <Form.Control
+                  type="number"
+                  placeholder="Enter discount"
+                  value={data.discount}
+                  onChange={(e) =>
+                    setData({ ...data, discount: e.target.value })
+                  }
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicDate">
+                <Form.Label>Date</Form.Label>
+                <Form.Control
+                  type="date"
+                  placeholder="Enter date"
+                  value={data.date}
+                  onChange={(e) => setData({ ...data, date: e.target.value })}
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+              <Button variant="danger" onClick={handleCancel} className="mx-2">
+                Cancel
+              </Button>
+            </Form>
+          </>
+        )}
+
+        {!showAddForm && <SettleAdvancedBP />}
       </div>
     </div>
   );
---
+++ client/src/pages/transaction/SettleAdvancedBP.jsx
@@ -0,0 +1,195 @@
+import React, { useState } from "react";
+import { Form, Button, Table } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import Alert from "react-bootstrap/Alert";
+
+function SettleFormSettleAdvancedBP() {
+  const [searchInvoiceNumber, setSearchInvoiceNumber] = useState("");
+  const [customerID, setCustomerID] = useState("");
+  const [filteredRecords, setFilteredRecords] = useState([]);
+  const [selectedInvoiceNumber, setSelectedInvoiceNumber] = useState("");
+  const [settleAmount, setSettleAmount] = useState("");
+  const [isSearchPerformed, setIsSearchPerformed] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [description, setDescription] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setIsSearchPerformed(false);
+    axios
+      .post("http://localhost:5000/advancebpsale/filterManualInvoice", {
+        manual_invoice_id: searchInvoiceNumber,
+      })
+      .then((res) => {
+        // console.log(res.data.sucess);
+        if (res.data.sucess) {
+          const data = res.data.result[0];
+          if (data) {
+            setFilteredRecords(data);
+            setIsSearchPerformed(true);
+            setShowAlert(false);
+            setSelectedInvoiceNumber(data.manual_invoice_id);
+            setCustomerID(data.customer_id);
+          } else {
+            setShowAlert(true);
+            console.log("No records found for the provided invoice number.");
+            setFilteredRecords([]);
+          }
+        } else {
+          setShowAlert(true);
+          console.error("Failed to fetch records:", response.status);
+        }
+      })
+
+      .catch((error) => {
+        console.error("Error occurred while fetching records:", error);
+        setShowAlert(true);
+        setFilteredRecords([]);
+      });
+
+    const inputValue = searchInvoiceNumber.trim();
+    setSearchInvoiceNumber(inputValue);
+
+    if (inputValue === "") {
+      setFilteredRecords([]);
+    }
+  };
+
+  const handleSettleAmountChange = (e) => {
+    const { value } = e.target;
+    setSettleAmount(value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    const { value } = e.target;
+    setDescription(value);
+  };
+
+  const handleSettle = (e) => {
+    e.preventDefault();
+
+    if (selectedInvoiceNumber && settleAmount && customerID) {
+      const settleData = {
+        invoice_id: selectedInvoiceNumber,
+        settle_amount: settleAmount,
+        customer_id: customerID,
+        description: description,
+      };
+
+      axios
+        .post(
+          "http://localhost:5000/advancedbpsale/settleInvoice",
+          settleData,
+          {
+            headers: { Authorization: "key " + sessionToken },
+          }
+        )
+        .then((response) => {
+          console.log("Settle API response:", response.data);
+
+          setSettleAmount("");
+          setCustomerID("");
+          setSelectedInvoiceNumber("");
+        })
+        .catch((error) => {
+          console.error("Error occurred during settle API call:", error);
+
+          setSettleAmount("");
+          setCustomerID("");
+          setSelectedInvoiceNumber("");
+        });
+    } else {
+      alert("Please fill all the required fields before settling the payment.");
+    }
+  };
+
+  const handleCancel = () => {
+    navigate("/transaction");
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSearch}>
+        <h3 className="text-center">Settle Payment</h3>
+        <Form.Group className="mb-3" controlId="formBasicSearchInvoiceNumber">
+          <Form.Label>Search by Bill Number</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Bill number (without space)"
+            value={searchInvoiceNumber}
+            onChange={(e) => setSearchInvoiceNumber(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Search
+        </Button>
+      </Form>
+
+      {showAlert && (
+        <Alert variant="danger">
+          <strong>
+            No sales found with the provided bill number. Please provide valid
+            bill number
+          </strong>
+        </Alert>
+      )}
+
+      {isSearchPerformed && filteredRecords && (
+        <>
+          <h4 className="text-center">AdvanceBP Sales Information</h4>
+          <p>Customer ID: {filteredRecords.customer_id}</p>
+          <p>Description: {filteredRecords.description}</p>
+          <p>Bill Amount: {filteredRecords.bill_amount}</p>
+          <p>Advance amount: {filteredRecords.advance_amount}</p>
+          <p>Discount: {filteredRecords.discount}</p>
+          <p>Date: {filteredRecords.date}</p>
+        </>
+      )}
+
+      {isSearchPerformed && (
+        <Form onSubmit={handleSettle}>
+          <h3 className="text-center">
+            Settle Payment for Invoice: {selectedInvoiceNumber}
+          </h3>
+          <Form.Group className="mb-3" controlId="formBasicSettleAmount">
+            <Form.Label>
+              Enter Settle Amount <span style={{ color: "red" }}>*</span>
+            </Form.Label>
+            <Form.Control
+              type="text"
+              name="settleAmount"
+              placeholder="Enter settle amount"
+              value={settleAmount}
+              onChange={handleSettleAmountChange}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicDescription">
+            <Form.Label>Enter Description (Optional)</Form.Label>
+            <Form.Control
+              type="text"
+              name="description"
+              placeholder="Enter description"
+              value={description}
+              onChange={handleDescriptionChange}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Settle
+          </Button>
+          <Button variant="danger" onClick={handleCancel} className="mx-2">
+            Cancel
+          </Button>
+        </Form>
+      )}
+    </>
+  );
+}
+
+export default SettleAdvancedBP;
