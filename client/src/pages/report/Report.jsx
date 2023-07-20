3a3f4071adba5b1bbfe4f9fb779dbe7fbc32a4c4
TJSangaran <thevarasasangaran@gmail.com>
2023-07-21 00:05:30 +0530

    Report page updated

--- client/src/pages/report/Report.jsx
+++ client/src/pages/report/Report.jsx
@@ -4,32 +4,57 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function Report() {
-  const [employeeData, setEmployeeData] = useState([]);
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
-
-  useEffect(() => {
-    axios
-      .get(import.meta.env.VITE_API_URL + "/report/showAll")
-      .then((res) => {
-        setEmployeeData(res.data.result);
-      })
-      .catch((error) => {
-        console.log("Error fetching creditSale data:", error);
-      });
-  });
-
+  const [transactionData, setTransactionData] = useState([]);
+  const [date, setDate] = useState("");
+  // const [endDate, setEndDate] = useState("");
   // Your data array
-  const data = [
-    // { id: 1, date: "2022-08-15", name: "Event 1" },
-    // { id: 2, date: "2022-08-20", name: "Event 2" },
-    // { id: 3, date: "2022-08-25", name: "Event 3" },
-    // ...
-  ];
+  const data = [];
+
+  // const handleDateChange = (e) => {
+  //   setDate(e.target.value);
+  // };
 
   const handleDateChange = (e) => {
-    setDate(e.target.value);
+    const selectedDate = new Date(e.target.value);
+
+    const year = selectedDate.getFullYear();
+    const month = String(selectedDate.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+    const day = String(selectedDate.getDate()).padStart(2, "0");
+
+    const formattedDate = `${year}-${month}-${day}`;
+
+    setDate(formattedDate);
   };
+
+  useEffect(() => {
+    // This block will run whenever the 'date' state is updated
+
+    // Make the API call here using the updated 'date' value
+    axios
+      .post("http://localhost:5000/report/getSales", { date: date })
+      .then((res) => {
+        // Handle the API response here
+        setTransactionData(res.data.result);
+      })
+      .catch((err) => {
+        // Handle errors
+      });
+  });
+  // Identify the transaction type
+  // const transactionFilter = ({ data }) => {
+  // if(data.type_id==ad)
+  // const handleDateChange = (e) => {
+  //   setDate(e.target.value);
+  //   console.log(date);
+  //   axios
+  //     .post("http://localhost:5000/report/getSale", date)
+  //     .then((res) => {
+  //       // console.log(res.data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // };
 
   //   const handleEndDateChange = (e) => {
   //     setEndDate(e.target.value);
@@ -40,8 +65,36 @@ function Report() {
     if (!startDate || !endDate) {
       return true; // If start or end date is not provided, return all items
     }
-    return item.date >= startDate && item.date <= endDate;
   });
+
+  let balance = 0;
+  const calculateBalance = ({ data }) => {
+    balance += data.amount - 2 * data.balance;
+    return balance;
+  };
+
+  // let totCredit = 0;
+  // const calculateTotCreadit = ({ data }) => {
+  //   totCredit += data.balance;
+  // };
+
+  const capitalize = (value) => {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+  };
+  const getDateFromTimestamp = (data) => {
+    const dateObj = new Date(data.date);
+
+    const year = dateObj.getFullYear();
+    const month = String(dateObj.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed, so add 1 and pad with '0'
+    const day = String(dateObj.getDate()).padStart(2, "0");
+
+    return `${year}-${month}-${day}`;
+  };
+
+  const bgChanger = (data) => {
+    const bg = data.balance > 0 ? "bg-danger" : "bg-success";
+    return bg;
+  };
 
   return (
     <div>
@@ -51,38 +104,10 @@ function Report() {
           <input
             // className="form-control"
             type="date"
-            value={Date}
+            value={date}
             onChange={handleDateChange}
           />
         </label>
-        {/* <label>
-          Start Date:
-          <input
-            // className="form-control"
-            type="date"
-            value={startDate}
-            onChange={handleStartDateChange}
-          />
-        </label> */}
-        {/* <label>
-          Start Date:
-          <input
-            // className="form-control"
-            type="date"
-            value={startDate}
-            onChange={handleStartDateChange}
-          />
-        </label> */}
-
-        {/* <label>
-          End Date:
-          <input
-            // className="form-control"
-            type="date"
-            value={endDate}
-            onChange={handleEndDateChange}
-          />
-        </label> */}
 
         <ul>
           {filteredData.map((item) => (
@@ -98,25 +123,38 @@ function Report() {
           <thead>
             <tr>
               <th>Bill No</th>
+              <th>Date</th>
               <th>Transaction Type</th>
-              <th>Description</th>
-              <th>Income</th>
-              <th>Expense</th>
+              {/* <th>Description</th> */}
+              <th>Cash</th>
+              <th>Credit</th>
               <th>Balance</th>
             </tr>
           </thead>
           <tbody>
             {/* Table rows with data */}
-            {employeeData.map((data) => (
-              <tr key={data.employee_id}>
-                <td>{data.employee_name}</td>
-                <td>{data.email}</td>
-                <td>{data.address}</td>
-                <td>{data.mobile}</td>
-                <td>{data.nic}</td>
-                <td>{data.age}</td>
+            {transactionData.map((data) => (
+              <tr
+                className={bgChanger(data)}
+                key={data.invoice_id + data.type_id}
+              >
+                <td>{data.manual_invoice_id}</td>
+                <td>{getDateFromTimestamp(data)}</td>
+                <td>{capitalize(data.type)}</td>
+                {/* <td>{data.cutomername}</td> */}
+                <td>{data.amount - data.balance}</td>
+                <td>{data.balance}</td>
+                <td>{calculateBalance({ data })}</td>
               </tr>
             ))}
+            <tr>
+              <td>#</td>
+              <td colSpan={2}>Total</td>
+              <td>{totCredit}</td>
+              <td>@twitter</td>
+
+              <td>{balance}</td>
+            </tr>
           </tbody>
         </table>
       </div>
