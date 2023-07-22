724f5fa3fef31dad23adad958d77d36ab5b88282
Thanu16 <kanagarajahthanushanth@gmail.com>
2023-07-22 16:51:28 +0530

    change employe table age to dob

---
+++ server/routerComponent/pages/CheckingEditAmount/CheckBalance.js
@@ -0,0 +1,15 @@
+class CheckBalance{
+    constructor(billAmount, oldBillAmount,discount, oldDisCount, balance, advanceAmount, OldAdvanceAmount){
+        this.billAmount = billAmount;
+        this.oldBillAmount = oldBillAmount;
+        
+    }
+
+   IsBalanceChange(billAmount, oldBillAmount){
+    }
+}
+
+    
+
+
+export default CheckBalance;
