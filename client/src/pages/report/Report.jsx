0080935a1be7e222bd24d619725193e9b3663c12
TJSangaran <thevarasasangaran@gmail.com>
2023-07-24 16:01:50 +0530

    Add summary table in report

--- client/src/pages/report/Report.jsx
+++ client/src/pages/report/Report.jsx
@@ -57,19 +57,27 @@ function Report() {
 
   let totbalance = 0;
   const calculateBalance = ({ data }) => {
-    totbalance += data.settle_amount - data.balance - data.return_payment;
+    if (data.start_transection) {
+      totbalance += data.settle_amount - data.balance - data.return_payment;
+    } else {
+      totbalance += data.settle_amount - data.return_payment;
+    }
     return totbalance;
   };
 
+  let totcash = 0;
+  let totret = 0;
   let totcred = 0;
-  // const calcualteCredit = ({ data }) => {
-  //   totcred += data.balance;
-  //   return totcred;
-  // };
+  let totbal = 0;
 
-  const capitalize = (value) => {
-    return (value.charAt(0).toUpperCase() + value.slice(1)).replace(/_/g, " ");
-  };
+  transactionData.map((data) => {
+    totcash += data.settle_amount;
+    totcred += data.start_transection ? data.balance : 0;
+    totret += data.return_payment;
+    totbal += data.start_transection
+      ? data.settle_amount - data.balance - data.return_payment
+      : data.settle_amount - data.return_payment;
+  });
 
   const getDate = (data) => {
     const dateObj = new Date(data.date);
@@ -124,7 +132,7 @@ function Report() {
         style={{ width: "100%" }}
       >
         <>
-          <main className="m-2 p-2 xl:max-w-4xl xl:mx-auto bg-white rounded-shadow">
+          <main className="m-5 p-4 xl:max-w-4xl xl:mx-auto bg-white rounded-shadow">
             <div>
               <header className="header-container  xl:flex-row">
                 <div className="font-bold uppercase tracking-wide text-4xl mb-3">
@@ -135,73 +143,94 @@ function Report() {
                       alt="Aivha Logo"
                     />
                   </h1>
+                  <div>
+                    <ul>
+                      <li>
+                        <strong>Start Date :</strong> {startDate.slice(0, 10)}
+                      </li>
+                      <li>
+                        <strong>End Date :</strong> {endDate.slice(0, 10)}
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-
-                {/* <div className="table-container">
-                  <table className="table table-bordered table-striped table-hover ">
-                    <thead>
-                      <tr>
-                        <th>Total Cred</th>
-                        <th> Total Cash</th>
-                        <th>Total Return</th>
-                        <th>Total Balance</th>
-                      </tr>
-                    </thead>
-                  </table>
-                </div> */}
-
                 <div className="flex flex-col items-end justify-end ">
-                  <ul>
-                    <li>
-                      <strong>Start Date :</strong> {startDate.slice(0, 10)}
-                    </li>
-                    <li>
-                      <strong>End Date :</strong> {endDate.slice(0, 10)}
-                    </li>
-                  </ul>
+                  <div className="table-container">
+                    <h5>Summary</h5>
+                    <table className="table table-bordered table-striped table-hover">
+                      <tbody>
+                        <tr>
+                          <th width="200">Total Cash</th>
+                          <td width="200" className="text-success">
+                            {totcash}
+                          </td>
+                        </tr>
+                        <tr>
+                          <th width="250">Total Credit</th>
+                          <td width="200" className="text-danger">
+                            {totcred}
+                          </td>
+                        </tr>
+                        <tr>
+                          <th width="250">Total Return</th>
+                          <td width="200" className="text-danger">
+                            {totret}
+                          </td>
+                        </tr>
+                        <tr>
+                          <th width="280">Total Balance</th>
+                          <td width="200" className="lable">
+                            {totbal}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
               </header>
             </div>
             <h3>Transaction Report</h3>
 
-            <table className="table table-bordered table-striped table-hover ">
-              <thead>
-                <tr>
-                  <th>Bill No</th>
-                  <th width="100">Date</th>
-                  <th>Transaction Type</th>
-                  <th width="100">Cash</th>
-                  <th width="100">Credit</th>
-                  <th width="100">Paid Amount</th>
-                  <th width="100">Balance</th>
-                </tr>
-              </thead>
-              <tbody>
-                {transactionData.map((data) => (
-                  <tr key={data.invoice_id + data.type_id}>
-                    <td>{data.manual_invoice_id}</td>
-                    <td>{getDate(data)}</td>
-                    <td>
-                      {(
-                        data.description.charAt(0).toUpperCase() +
-                        data.description.slice(1)
-                      ).replace(/_/g, " ")}
-                    </td>
-                    <td className="text-success">
-                      {data.settle_amount ? data.settle_amount : ""}
-                    </td>
-
-                    <td className="text-danger">
-                      {data.start_transection == 1 ? data.balance : ""}
-                    </td>
-                    <td className="text-danger">
-                      {data.return_payment ? data.return_payment : ""}
-                    </td>
-                    <td className="lable">{calculateBalance({ data })}</td>
+            <div>
+              <table className="table table-bordered table-striped table-hover ">
+                <thead>
+                  <tr>
+                    <th>Bill No</th>
+                    <th width="150">Date</th>
+                    <th>Transaction Type</th>
+                    <th width="80">Cash</th>
+                    <th width="80">Credit</th>
+                    <th width="80">Return</th>
+                    <th width="90">Balance</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {transactionData.map((data) => (
+                    <tr key={data.invoice_id + data.type_id}>
+                      <td>{data.manual_invoice_id}</td>
+                      <td>{getDate(data)}</td>
+                      <td>
+                        {(
+                          data.description.charAt(0).toUpperCase() +
+                          data.description.slice(1)
+                        ).replace(/_/g, " ")}
+                      </td>
+                      <td className="text-success">
+                        {data.settle_amount ? data.settle_amount : ""}
+                      </td>
+
+                      <td className="text-danger">
+                        {data.start_transection == 1 ? data.balance : ""}
+                      </td>
+                      <td className="text-danger">
+                        {data.return_payment ? data.return_payment : ""}
+                      </td>
+                      <td className="lable">{calculateBalance({ data })}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </main>
         </>
       </div>
