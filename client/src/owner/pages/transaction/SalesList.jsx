110ffda8c191552443113f65c8b7463469e43a8b
Pairavi <pairavi.20@cse.mrt.ac.lk>
2023-08-02 08:26:57 +0530

    edittable changes

--- client/src/owner/pages/transaction/SalesList.jsx
+++ client/src/owner/pages/transaction/SalesList.jsx
@@ -1,46 +1,63 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import CommonTable from "./Table";
 
-const columns = ["Bill Number", "Type", "Amount", "Date", "CustomerName"];
+function SalesList() {
+  const [editTransactionData, setEditTransactionData] = useState([]);
 
-const SalesList = () => {
-  const [salesData, setSalesData] = useState([
-    {
-      "Bill Number": "1234",
-      Type: "Cash",
-      Amount: 100,
-      Date: "2023-07-18",
-      CustomerName: "John Doe",
-    },
-    {
-      "Bill Number": "5678",
-      Type: "Credit",
-      Amount: 150,
-      Date: "2023-07-19",
-      CustomerName: "Jane Smith",
-    },
-  ]);
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+  const day = currentDate.getDate().toString().padStart(2, "0");
+  const date = `${year}-${month}-${day}`;
 
-  // useEffect(() => {
-  //   // Fetch data from the backend API
-  // For demonstration purposes, we'll use the sampleSalesData as the response
-  // Remove the sampleSalesData and uncomment the axios code when connecting to the backend.
-  // axios.get("/api/sales")
-  //   .then((response) => {
-  //     setSalesData(response.data);
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error fetching data:", error);
-  //   });
+  useEffect(() => {
+    const sessionToken = localStorage.getItem("sessionToken");
+    console.log(sessionToken);
 
-  //   fetchCashSalesData();
-  // }, []);
+    axios
+      .post(
+        "http://localhost:5000/dashboard/editedSales",
+        { date: date },
+        {
+          headers: { Authorization: "key " + sessionToken },
+        }
+      )
+      .then((res) => {
+        setEditTransactionData(res.data.result);
+        console.log(res.data.result);
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log("Error fetching editSale data:", error);
+      });
+  }, []);
+
+  const editTransactionColumns = [
+    "manual_invoice_id",
+    "customer_name",
+    "type_id",
+    "description",
+    "bill_amount",
+    "advance_amount",
+    "discount",
+    "amount",
+    "balance",
+    "return_payment",
+  ];
 
   return (
-    <div>
-      <CommonTable data={salesData} columns={columns} />
+    <div className="px-5 py-3">
+      <div className="mt-4 px-2 pt-5">
+        <h3>Edit Transaction List</h3>
+        {/* Use the CommonTable component to display the table */}
+        <CommonTable
+          data={editTransactionData}
+          columns={editTransactionColumns}
+        />
+      </div>
     </div>
   );
-};
+}
 
 export default SalesList;
